04e3b828ae647b19bfccface822a23754cdd7a0e
Filip Lundborg <filip@filipl.se>
2014-12-06 14:02:15 +0100

    Add script, example message and updated readme.

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
-secret-santa-lottery
+Secret Santa Lottery
 ====================
 
 Small utility to decide who to give presents to in a secret santa game.
+
+Example Usage
+-------------
+
+```
+python secret_santa.py --message-file example_message.txt "John Doe <john@example.com>" "Jane Doe <jane@example.com>"
+```
+
+Help
+----
+
+```
+usage: secret_santa.py [-h] [--smtp SMTP] [--sender SENDER]
+                       [--subject SUBJECT] --message-file PATH
+                       [--ignore-warnings]
+                       person [person ...]
+
+Sceret Santa Lottery
+
+positional arguments:
+  person               A participant in the lottery - "Name <email>"
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --smtp SMTP          SMTP server
+  --sender SENDER      Sender email address
+  --subject SUBJECT    Mail subject
+  --message-file PATH  Message file
+  --ignore-warnings    Ignore warnings about the message file
+```
---
+++ secret_santa.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import random
+import smtplib
+from email.mime.text import MIMEText
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description="Sceret Santa Lottery")
+
+def participant(str):
+    ms = re.match("(.*) <(.*)>", str)
+    if not ms:
+        raise argparse.ArgumentTypeError("%s is not a person" % (str))
+    return ms.groups()
+
+def body_file(filename):
+    try:
+        body = open(filename, "r").read()
+    except FileNotFoundError:
+        raise argparse.ArgumentTypeError("unable to find file %s" % (filename))
+    return body
+
+parser.add_argument("persons", metavar="person", nargs="+",
+    help="A participant in the lottery - \"Name <email>\"", type=participant)
+parser.add_argument("--smtp", default="localhost", help="SMTP server")
+parser.add_argument("--sender", default="lottery@example.com",
+    help="Sender email address")
+parser.add_argument("--subject", default="Lottery!", help="Mail subject")
+parser.add_argument("--message-file", type=body_file, help="Message file",
+    dest="path", required=True)
+parser.add_argument("--ignore-warnings", dest="ignore_warns", action='store_true',
+    help="Ignore warnings about the message file")
+
+args = parser.parse_args()
+
+SMTP_SERVER = args.smtp
+MAIL_SENDER = args.sender
+MAIL_SUBJECT = args.subject
+MAIL_BODY = args.path
+
+if not args.ignore_warns:
+    for p in ["{giver_name}", "{receiver_name}"]:
+        if p not in MAIL_BODY:
+            print("warning: '%s' not used in message." % (p,))
+            exit(1)
+
+givers = args.persons[:]
+receivers = givers[:]
+
+random.seed()
+
+def get_receiver(giver):
+    global receivers
+    candidates = list(filter(lambda r: r != giver, receivers))
+    if len(candidates) == 0:
+        print("only one person left in lottery with noone to give to - try again.")
+        exit(1)
+    receiver = random.choice(candidates)
+    receivers = list(filter(lambda r: r != receiver, receivers))
+    return receiver
+
+matches = {}
+for giver in givers:
+    receiver = get_receiver(giver)
+    matches[giver] = receiver
+
+def mail_person(giver, receiver):
+    me = MAIL_SENDER
+    body = MAIL_BODY.format(giver_name=giver[0], receiver_name=receiver[0])
+    msg = MIMEText(body)
+    msg['Subject'] = MAIL_SUBJECT
+    msg['From'] = me
+    msg['To'] = giver[1]
+
+    s = smtplib.SMTP(SMTP_SERVER)
+    s.sendmail(me, [giver[1]], msg.as_string())
+    s.quit()
+
+for g, r in matches.items():
+    mail_person(g, r)
+
+print("done.")
